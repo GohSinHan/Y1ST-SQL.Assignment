6ac9c095b63d9f4ce6c541375e338085b00faa4c
Michael <michaelgwj@5e604d9b56f1>
2025-06-23 13:20:06 +0000

    Create template folder for html files, and added py files for backend logic

---
+++ TCX2003_db_project.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, request, redirect, session
+import mysql.connector
+import hashlib
+from uuid import uuid4
+from datetime import datetime
+import os
+# import logging # Write logs in PythonAnywhere
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'TCX2003_Secret_Key'
+config_path = os.path.join(os.path.dirname(__file__), 'my.ini')
+
+# logging.basicConfig(level=logging.INFO)
+
+@app.route('/')
+def home():
+    return redirect('/login')  # Redirects to the 'login' route
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    status_msg = None
+    if request.method == "POST":
+        # Process login data
+        user_name = request.form['txt_username']
+
+        # Hash password
+        m = hashlib.md5()
+        m.update(request.form['txt_password'].encode('UTF-8'))
+        password = m.hexdigest()
+
+        try:
+            status_msg = "Attempting DB connection ... "
+            cnx = mysql.connector.connect(option_files = config_path)
+            status_msg += "Connection successful"
+
+            cursor = cnx.cursor()
+            status_msg += "Cursor Created"
+            cursor.execute(
+                'SELECT Student_ID FROM Student WHERE Email=%s AND Password_Hash=%s',
+                (user_name, password)
+            )
+            result = cursor.fetchone()
+
+            if(result):
+                student_id = result[0]
+
+                # Create session
+                session_token = str(uuid4())
+                session['session_token'] = session_token
+                session['student_id'] = student_id
+
+                # Add to session DB
+                cursor.execute(
+                    "INSERT INTO Session (Session_Token, Student_ID, Login_Time) VALUES (%s, %s, %s)",
+                    (session_token, student_id, datetime.now())
+                )
+                cnx.commit()
+
+                # Close connection
+                cursor.close()
+                cnx.close()
+
+                # redirect to dashboard
+                return redirect("/dashboard")
+            else:
+                error = "Invalid Credentials. Please try again."
+        except Exception as e:
+            print("Login error:", e)
+            error = "Internal server error."
+            status_msg = f"Exception occurred!: {e}"
+
+    # direct to login page upon failed login
+    return render_template('login.html', error=error, status_msg=status_msg)
+
+@app.route('/dashboard')
+def dashboard():
+    if 'student_id' not in session:
+        return redirect('/login')
+
+    return render_template("dashboard.html")
+
+@app.route('/logout')
+def logout():
+    error = None
+    try:
+        session_token = session.get('session_token')
+        if session_token:
+            cnx = mysql.connector.connect(option_files = config_path)
+            cursor = cnx.cursor()
+            cursor.execute(
+                'UPDATE Session SET Is_Active = %s WHERE Session_Token = %s',
+                (False, session_token)
+            )
+            cnx.commit()
+            cursor.close()
+            cnx.close()
+    except Exception as e:
+        error = f"Exception occurred! {e}"
+
+    # Clear Flask session and redirect
+    session.clear()
+    return redirect('/login')
